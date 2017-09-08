4a592cc06524e250bc9cd58aaa5948a8471bc017
yamahigashi <yamahigashi@gmail.com>
2017-09-08 15:37:31 +0900

    maya: make deploy zip as maya module.

--- Plugin/build_maya.bat
+++ Plugin/build_maya.bat
@@ -4,25 +4,26 @@ set MAYA_VERSION=2016
 set MAYA_LIB_DIR=%cd%\External\Maya2016\lib
 set MAYA_INCLUDE_DIR=%cd%\External\Maya2016\include
 msbuild MeshSyncClientMaya.vcxproj /t:Build /p:Configuration=Master /p:Platform=x64 /m /nologo
-mkdir "UnityMeshSync for Maya\Maya2016"
-copy _out\MeshSyncClientMaya2016_x64_Master\MeshSyncClientMaya.mll "UnityMeshSync for Maya\Maya2016"
-copy MeshSyncClientMaya\MEL\*.mel "UnityMeshSync for Maya\Maya2016"
+mkdir "UnityMeshSync for Maya\Maya2016\plug-ins"
+copy _out\x64_Master\MeshSyncClientMaya2016\MeshSyncClientMaya.mll "UnityMeshSync for Maya\Maya2016\plug-ins"
 
 set MAYA_VERSION=2016.5
 set MAYA_LIB_DIR=%cd%\External\Maya2016.5\lib
 set MAYA_INCLUDE_DIR=%cd%\External\Maya2016.5\include
 msbuild MeshSyncClientMaya.vcxproj /t:Build /p:Configuration=Master /p:Platform=x64 /m /nologo
-mkdir "UnityMeshSync for Maya\Maya2016.5"
-copy _out\MeshSyncClientMaya2016.5_x64_Master\MeshSyncClientMaya.mll "UnityMeshSync for Maya\Maya2016.5"
-copy MeshSyncClientMaya\MEL\*.mel "UnityMeshSync for Maya\Maya2016.5"
+mkdir "UnityMeshSync for Maya\Maya2016.5\plug-ins"
+copy _out\x64_Master\MeshSyncClientMaya2016.5\MeshSyncClientMaya.mll "UnityMeshSync for Maya\Maya2016.5\plug-ins"
 
 set MAYA_VERSION=2017
 set MAYA_LIB_DIR=%cd%\External\Maya2017\lib
 set MAYA_INCLUDE_DIR=%cd%\External\Maya2017\include
 msbuild MeshSyncClientMaya.vcxproj /t:Build /p:Configuration=Master /p:Platform=x64 /m /nologo
-mkdir "UnityMeshSync for Maya\Maya2017"
-copy _out\MeshSyncClientMaya2017_x64_Master\MeshSyncClientMaya.mll "UnityMeshSync for Maya\Maya2017"
-copy MeshSyncClientMaya\MEL\*.mel "UnityMeshSync for Maya\Maya2017"
+mkdir "UnityMeshSync for Maya\Maya2017\plug-ins"
+copy _out\x64_Master\MeshSyncClientMaya2017\MeshSyncClientMaya.mll "UnityMeshSync for Maya\Maya2017\plug-ins"
+
+mkdir "UnityMeshSync for Maya\scripts"
+copy MeshSyncClientMaya\MEL\*.mel "UnityMeshSync for Maya\scripts"
+copy MeshSyncClientMaya\MEL\*.mod "UnityMeshSync for Maya"
 
 del "UnityMeshSync for Maya.zip"
 powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('UnityMeshSync for Maya', 'UnityMeshSync for Maya.zip'); }"
